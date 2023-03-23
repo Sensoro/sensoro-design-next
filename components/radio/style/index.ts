ed67127da111453c9f3b6dc8c7b976e7cd2e7704
wangxingkang <wang_xingkang@qq.com>
2023-03-23 20:11:32 +0800

    chore: 修复启动脚本报错

--- components/radio/style/index.ts
+++ components/radio/style/index.ts
@@ -1,2 +1,50 @@
-// ============================== Tokens ==============================
+import { genComponentStyleHook, mergeToken } from '../../theme/internal';
+import { resetComponent } from '../../style';
+
+import type { FullToken, GenerateStyle } from '../../theme/internal';
+
 export interface ComponentToken {}
+
+interface RadioToken extends FullToken<'Radio'> {
+  radioCls: string;
+  radioSize: number;
+}
+
+// ============================== Styles ==============================
+export const genRadioStyle: GenerateStyle<RadioToken> = (token) => {
+  const { radioCls } = token;
+  const wrapperCls = `${radioCls}-wrapper`;
+
+  return [
+    {
+      // Group
+      [`${radioCls}-group`]: {
+        ...resetComponent(token),
+
+        display: 'inline-flex',
+      },
+
+      // Wrapper
+      [wrapperCls]: {
+        ...resetComponent(token),
+
+        display: 'inline-flex',
+        alignItems: 'baseline',
+        cursor: 'pointer',
+      }
+    },
+  ];
+}
+
+
+// ============================== Export ==============================
+export default genComponentStyleHook('Radio', (token, { prefixCls }) => {
+  const radioToken: RadioToken = mergeToken<RadioToken>(token, {
+    radioCls: `.${prefixCls}`,
+    radioSize: token.controlInteractiveSize,
+  });
+
+  return [
+    genRadioStyle(radioToken),
+  ]
+});
