906b0c42254e829108a82bc116a7ba24c0ddaac9
wangxingkang <wang_xingkang@qq.com>
2023-03-28 13:23:05 +0800

    docs: update site style

--- .dumi/theme/common/GlobalStyles.tsx
+++ .dumi/theme/common/GlobalStyles.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { ColorStyle } from './Color/ColorStyle';
-import { Markdown, Common } from './styles';
+import { Markdown, Common, Demo, Highlight, SearchBar } from './styles';
 
 export const GlobalStyles: React.FC = () => (
   <>
     <Common />
+    <Highlight />
     <Markdown />
+    <SearchBar />
+    <Demo />
     <ColorStyle />
   </>
 )
--- .dumi/theme/common/styles/index.ts
+++ .dumi/theme/common/styles/index.ts
@@ -1,2 +1,5 @@
 export { Common } from './Common';
+export { Demo } from './Demo';
 export { Markdown } from './Markdown';
+export { Highlight } from './Highlight';
+export { SearchBar } from './SearchBar';
