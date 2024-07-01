c398dcbc9264454f41aa4ee493c16c76452cf4a9
wangxingkang <wang_xingkang@qq.com>
2024-07-01 11:05:44 +0800

    test: add test

---
+++ packages/react/src/scroll-area/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+import ScrollArea from '../';
+
+describe('scroll-area', () => {
+  it('should be defined ', () => {
+    expect(ScrollArea).toBeDefined();
+  });
+});
