afddb8ffa7294a886e00f5f8fb7cea197a1619a0
wangxingkang <wang_xingkang@qq.com>
2024-07-04 13:14:13 +0800

    chore: add __dirname

--- scripts/.internal/constants.ts
+++ scripts/.internal/constants.ts
@@ -1,14 +1,10 @@
 import { join } from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
+import { __dirname } from '../helpers/utils/url'
 
 const ROOT = join(__dirname, '../../')
 
 export const PATHS = {
   ROOT,
   PACKAGES: join(ROOT, 'packages'),
-  DESIGN: join(ROOT, 'packages', 'lotus-design'),
+  DESIGN: join(ROOT, 'packages', '@lotus-design/react'),
 } as const
-
-export const PLUGINS_SERVER_PORT = 9100
