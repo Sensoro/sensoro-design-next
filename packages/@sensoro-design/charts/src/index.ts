daae24966f51062f30862ba22612721e1101dd3e
wangxingkang <wang_xingkang@qq.com>
2024-07-18 16:08:46 +0800

    chore: 修正导出

--- packages/@sensoro-design/charts/src/index.ts
+++ packages/@sensoro-design/charts/src/index.ts
@@ -1,5 +1,8 @@
 import './theme';
 
+export { default as Line } from './Line';
+export type { LineConfig } from './Line';
+
 export { default as Area } from './Area';
 export type { AreaConfig } from './Area';
 export { default as Bar } from './Bar';
@@ -10,8 +13,6 @@ export { default as Funnel } from './Funnel';
 export type { FunnelConfig } from './Funnel';
 export { default as Gauge } from './Gauge';
 export type { GaugeConfig } from './Gauge';
-// export { default as Line } from './Line';
-export type { LineConfig } from './Line';
 export { default as Pie } from './Pie';
 export type { PieConfig } from './Pie';
 export { default as Radar } from './Radar';
@@ -20,5 +21,3 @@ export { default as Rose } from './Rose';
 export type { RoseConfig } from './Rose';
 export { default as Treemap } from './Treemap';
 export type { TreemapConfig } from './Treemap';
-
-export { Line } from '@ant-design/plots';
