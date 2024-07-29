712b367d9cb96dea29a3f3c4811bda9ce5af8681
wangyuhu <wangyuhu@sensoro.com>
2024-07-29 18:48:46 +0800

    feat: 柱状图隐藏高亮区域，添加padding

--- packages/@sensoro-design/plots/src/components/Column/index.tsx
+++ packages/@sensoro-design/plots/src/components/Column/index.tsx
@@ -6,13 +6,23 @@ import { getItemConfig } from '../../helpers/utils';
 import { DEFAULT_INSET_LEFT, DEFAULT_INSET_RIGHT } from '../../config';
 import {
   DEFAULT_AXIS_CONFIG,
+  DEFAULT_INTERACTION_CONFIG,
+  DEFAULT_SCALE_CONFIG,
   DEFAULT_STYLE_CONFIG,
 } from './config';
-import type { AxisConfig, StyleConfig } from './types';
+import type {
+  AxisConfig,
+  InteractionConfig,
+  ScaleConfig,
+  StyleConfig,
+} from './types';
 
-export interface ColumnConfig extends Omit<AntColumnConfig, 'axis' | 'style'> {
+export interface ColumnConfig
+  extends Omit<AntColumnConfig, 'interaction' | 'scale' | 'axis' | 'style'> {
   axis?: AntColumnConfig['axis'] | boolean;
+  scale?: AntColumnConfig['scale'] | boolean;
   style?: AntColumnConfig['axis'] | boolean;
+  interaction?: AntColumnConfig['interaction'] | boolean;
 }
 
 export const Column = forwardRef<Chart, ColumnConfig>(
@@ -21,12 +31,19 @@ export const Column = forwardRef<Chart, ColumnConfig>(
       insetLeft = DEFAULT_INSET_LEFT,
       insetRight = DEFAULT_INSET_RIGHT,
       axis = true,
+      scale = true,
       style = true,
+      interaction = true,
       ...rest
     } = props;
 
     const styleConfig = getItemConfig<StyleConfig>(style, DEFAULT_STYLE_CONFIG);
     const axisConfig = getItemConfig<AxisConfig>(axis, DEFAULT_AXIS_CONFIG);
+    const scaleConfig = getItemConfig<ScaleConfig>(scale, DEFAULT_SCALE_CONFIG);
+    const interactionConfig = getItemConfig<InteractionConfig>(
+      interaction,
+      DEFAULT_INTERACTION_CONFIG,
+    );
 
     // TODO: 柱状图占比，背景宽度
     return (
@@ -34,7 +51,9 @@ export const Column = forwardRef<Chart, ColumnConfig>(
         insetLeft={insetLeft}
         insetRight={insetRight}
         axis={axisConfig}
+        scale={scaleConfig}
         style={styleConfig}
+        interaction={interactionConfig}
         {...rest}
         ref={ref}
       />
