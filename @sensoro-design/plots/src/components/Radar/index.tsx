37809cd42e239395700c719592f3f1849dffdf61
wangyuhu <wangyuhu@sensoro.com>
2024-08-06 18:44:38 +0800

    feat: 雷达图

---
+++ @sensoro-design/plots/src/components/Radar/index.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef } from 'react';
+import { isUndefined } from '@rcuse/shared';
+import { Radar as AntRadar } from '@ant-design/plots';
+import type { Chart } from '@ant-design/plots/es/interface';
+import { Renderer as SVGRenderer } from '@antv/g-svg';
+import type { RadarConfig as AntRadarConfig } from '@ant-design/plots';
+import { getItemConfig } from '../../helpers/utils';
+import { DEFAULT_INSET_LEFT, DEFAULT_INSET_RIGHT } from '../../config';
+import {
+  DEFAULT_AREA_CONFIG,
+  DEFAULT_AXIS_CONFIG,
+  DEFAULT_POINT_CONFIG,
+} from './config';
+import type { AreaConfig, PointConfig } from './types';
+
+export interface RadarConfig extends Omit<AntRadarConfig, 'area' | 'point' | 'axis'> {
+  area?: AntRadarConfig['area'] | boolean;
+  point?: AntRadarConfig['point'] | boolean;
+  axis?: AntRadarConfig['axis'] | boolean;
+}
+
+export const Radar = forwardRef<Chart, RadarConfig>(
+  (props, ref) => {
+    const {
+      insetLeft = DEFAULT_INSET_LEFT,
+      insetRight = DEFAULT_INSET_RIGHT,
+      area = true,
+      point = true,
+      axis = true,
+      ...rest
+    } = props;
+
+    const pointConfig = getItemConfig<PointConfig>(point, DEFAULT_POINT_CONFIG);
+    const axisConfig = getItemConfig<PointConfig>(axis, DEFAULT_AXIS_CONFIG);
+    const areaConfig = getItemConfig<AreaConfig>(area, DEFAULT_AREA_CONFIG);
+
+    return (
+      <AntRadar
+        insetLeft={insetLeft}
+        insetRight={insetRight}
+        area={areaConfig}
+        point={pointConfig}
+        axis={axisConfig}
+        {...rest}
+        ref={ref}
+        renderer={new SVGRenderer()}
+      />
+    );
+  },
+);
+
+export default Radar;
---
+++ packages/storybook/stories/Charts/Radar.stories.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Radar, type RadarConfig } from '@sensoro-design/plots';
+
+const data = [
+  { item: 'Design', type: 'a', score: 70 },
+  { item: 'Design', type: 'b', score: 30 },
+  { item: 'Development', type: 'a', score: 60 },
+  { item: 'Development', type: 'b', score: 70 },
+  { item: 'Marketing', type: 'a', score: 50 },
+  { item: 'Marketing', type: 'b', score: 60 },
+  { item: 'Users', type: 'a', score: 40 },
+  { item: 'Users', type: 'b', score: 50 },
+  { item: 'Test', type: 'a', score: 60 },
+  { item: 'Test', type: 'b', score: 70 },
+  { item: 'Language', type: 'a', score: 70 },
+  { item: 'Language', type: 'b', score: 50 },
+  { item: 'Technology', type: 'a', score: 50 },
+  { item: 'Technology', type: 'b', score: 40 },
+  { item: 'Support', type: 'a', score: 30 },
+  { item: 'Support', type: 'b', score: 40 },
+  { item: 'Sales', type: 'a', score: 60 },
+  { item: 'Sales', type: 'b', score: 40 },
+  { item: 'UX', type: 'a', score: 50 },
+  { item: 'UX', type: 'b', score: 60 },
+];
+
+const meta = {
+  title: 'Charts/Radar',
+};
+
+export default meta;
+
+export function Basic() {
+  const config: RadarConfig = {
+    title: '雷达图',
+    data,
+    xField: 'item',
+    yField: 'score',
+    colorField: 'type',
+    scale: {
+      y: { domainMax: 80 },
+    },
+    // TODO: radar坐标系问题处理
+    // coordinateType: 'radar',
+  };
+
+  return (
+    <Radar {...config} />
+  );
+}
