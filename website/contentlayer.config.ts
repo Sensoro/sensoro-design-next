e1d145d6c0d29be0f3d15387c332ad8bcd6c43f9
wangxingkang <wang_xingkang@qq.com>
2024-08-08 10:28:44 +0800

    docs: update contentlayer config

--- website/contentlayer.config.ts
+++ website/contentlayer.config.ts
@@ -1,8 +1,22 @@
 import { defineDocumentType, defineNestedType, makeSource } from 'contentlayer2/source-files';
 
+export const Doc = defineDocumentType(() => ({
+  name: 'Doc',
+  filePathPattern: `docs/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+  },
+}));
+
 export default makeSource({
   contentDirPath: './content',
-  documentTypes: [],
+  documentTypes: [
+    Doc,
+  ],
   mdx: {
 
   },
