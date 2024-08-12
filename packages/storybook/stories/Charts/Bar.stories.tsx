2f6bbd4c2e8a1fba43ef760b62c8f84e68ce3845
wangxingkang <wang_xingkang@qq.com>
2024-08-12 14:14:37 +0800

    docs(Bar): 调整示例高度计算

--- packages/storybook/stories/Charts/Bar.stories.tsx
+++ packages/storybook/stories/Charts/Bar.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Bar, type BarConfig } from '@sensoro-design/plots';
+import { backgroundColorNames } from 'chalk';
 
 const meta = {
   title: 'Charts/Bar',
@@ -7,19 +8,19 @@ const meta = {
 
 const data = [
   {
-    labelName: '蓝领',
+    name: '蓝领',
     value: 110,
   },
   {
-    labelName: '白领',
+    name: '白领',
     value: 220,
   },
   {
-    labelName: '制造业蓝领哈久啊禄口街道发了卡萨丁激发',
+    name: '制造业蓝领哈久啊禄口街道发了卡萨丁激发',
     value: 330,
   },
   {
-    labelName: '退休人员',
+    name: '退休人员',
     value: 400,
   },
 ];
@@ -29,8 +30,9 @@ export default meta;
 export function Basic() {
   const config: BarConfig = {
     title: '基础条形图',
+    height: data.length * 8 + (data.length - 1) * 24 + 92,
     data,
-    xField: 'labelName',
+    xField: 'name',
     yField: 'value',
   };
 
