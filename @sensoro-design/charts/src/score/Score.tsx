2bce4a1d961e671c45bb767b4f5da73ebb041541
wangxingkang <wang_xingkang@qq.com>
2024-08-15 14:00:00 +0800

    chore: update tsconfig.json

--- @sensoro-design/charts/src/score/Score.tsx
+++ @sensoro-design/charts/src/score/Score.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import type { ScoreConfig } from './interface';
 
-export function Score(
-  {
-    color = 'green',
-  }: ScoreConfig,
-) {
-  console.log(color);
-  return null;
+export function Score() {
+  return (
+    <div>Score</div>
+  );
 }
