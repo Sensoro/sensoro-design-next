ca119e34f95c4338aff9c06f25261441ee2e597c
wangxingkang <wang_xingkang@qq.com>
2024-08-20 17:04:50 +0800

    chore: add @sensoro-design/vchart

---
+++ @sensoro-design/vchart/src/circle-packing/index.ts
@@ -0,0 +1,4 @@
+import { CirclePackingChart } from '@visactor/react-vchart';
+
+export type { CirclePackingChartProps as CirclePackingProps } from '@visactor/react-vchart';
+export default CirclePackingChart;
---
+++ @sensoro-design/vchart/src/index.ts
@@ -0,0 +1,2 @@
+export { default as CirclePacking } from './circle-packing';
+export type { CirclePackingProps } from './circle-packing';
