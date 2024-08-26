3c0f8b7a4bb476887922012e4d025384d1b3552b
wangxingkang <wang_xingkang@qq.com>
2024-08-26 10:47:19 +0800

    chore: update next config

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -2,8 +2,15 @@ import { createContentlayerPlugin } from 'next-contentlayer2';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
 };
 
 const withContentlayer = createContentlayerPlugin({
