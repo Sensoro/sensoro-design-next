d4c3ec138764105b2b43ab6b39f06c30c09f7ec8
wangxingkang <wang_xingkang@qq.com>
2024-08-29 17:59:21 +0800

    Revert "chore: 优化发布脚本"
    This reverts commit 3a54b6b534301324d84a3b336f0c40df71faa049.

--- scripts/release.ts
+++ scripts/release.ts
@@ -8,18 +8,16 @@ async function release() {
   const branch = await git.branch();
   logger.info(`branch: ${branch.current}`);
 
-  logger.log('check git status');
   const status = await git.status();
   if (status.files.length !== 0) {
-    logger.error('git status is not clean');
+    logger.error('Working tree is not clean');
     process.exit(1);
   }
 
-  logger.log('check git remote update');
-  if (status.behind) {
-    logger.error('git status is behind remote`');
-    process.exit(1);
-  }
+  await git.fetch();
+  const status1 = await git.status({
+
+  });
 }
 
 release().catch(() => {
