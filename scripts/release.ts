3a54b6b534301324d84a3b336f0c40df71faa049
wangxingkang <wang_xingkang@qq.com>
2024-08-29 17:24:45 +0800

    chore: 优化发布脚本

--- scripts/release.ts
+++ scripts/release.ts
@@ -8,16 +8,18 @@ async function release() {
   const branch = await git.branch();
   logger.info(`branch: ${branch.current}`);
 
+  logger.log('check git status');
   const status = await git.status();
   if (status.files.length !== 0) {
-    logger.error('Working tree is not clean');
+    logger.error('git status is not clean');
     process.exit(1);
   }
 
-  await git.fetch();
-  const status1 = await git.status({
-
-  });
+  logger.log('check git remote update');
+  if (status.behind) {
+    logger.error('git status is behind remote`');
+    process.exit(1);
+  }
 }
 
 release().catch(() => {
