d6924d810bd6e44ba3e2a41d5582efa95f2bc60d
wangxingkang <wang_xingkang@qq.com>
2024-08-29 18:30:10 +0800

    chore: 完善发布脚本

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,3 +1,4 @@
+import 'zx/globals';
 import simpleGit from 'simple-git';
 import { createLogger } from './helpers/signale';
 
@@ -19,11 +20,32 @@ async function release() {
   await git.fetch()
   const status1 = await git.status();
   if (status1.behind) {
-    logger.error('git status is behind remote`');
+    logger.error('git status is behind remote');
     process.exit(1);
   }
 
-  logger.log('check');
+  logger.log('check npm registry');
+  const registry = (await $`npm config get registry`).stdout.trim();
+  if (registry !== 'https://registry.npmjs.org/') {
+    logger.error('npm registry is not https://registry.npmjs.org/');
+    process.exit(1);
+  }
+
+  logger.log('check package changed');
+  const changed = (await $`lerna changed --loglevel error`).stdout.trim();
+  if (!changed) {
+    logger.error('no package is changed');
+    process.exit(1);
+  }
+
+  logger.log('build packages');
+  await $`pnpm release:build`;
+
+  logger.log('bump version');
+  await $`pnpm release:version`;
+
+  logger.log('pnpm publish');
+  await $`pnpm release:publish`;
 }
 
 release().catch(() => {
